5ca7d83c8df5ad91116a2d09581bf36de6d077c3
Casey Billett <cbillett@xyu.ca>
2019-02-12 22:07:51 -0500

    Adding more tests & Jest debug support
    * Added some new server test cases
    * Added VSCode launch settings for debugging Jest tests
    * Adding simple Perl-based test for server
    * Include configuration for ESLint
    * Adding a license ;)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    projects: [
+        {
+            displayName: 'server',
+            testMatch: ['<rootDir>/server/**/*.test.js']
+        },
+        {
+            displayName: 'client',
+            testMatch: ['<rootDir>/client/**/*.test.js']
+        }
+    ]
+};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,47 +1,61 @@
 var app = require('../app');
 var request = require('supertest');
 
-describe("test server endpoint", () => {
+describe('test server endpoint', () => {
     test('should not have a post method', (done) => {
         return request(app).post('/primes/').expect(404, done);
     });
 
-    test("get with no request should not be found", (done) => {
+    test('get with no request should not be found', (done) => {
         return request(app).get('/primes/').expect(404, done);
     });
 
-    test("a prime should return true", (done) => {
+    test('a prime should return true', (done) => {
         request(app).get('/primes/7').expect(200).then((response) => {
             expect(response.body.result).toBe(true);
             done();
-        })
-    })
+        });
+    });
 
-    test("a compound should return false", (done) => {
+    test('a compound should return false', (done) => {
         request(app).get('/primes/6').expect(200).then((response) => {
             expect(response.body.result).toBe(false);
             done();
-        })
-    })
+        });
+    });
 
-    test("a negative should return false", (done) => {
+    test('a negative should return false', (done) => {
         request(app).get('/primes/-1').expect(200).then((response) => {
             expect(response.body.result).toBe(false);
             done();
-        })
-    })
+        });
+    });
 
-    test("a 64-bit prime returns true", (done) => {
+    test('a 64-bit prime returns true', (done) => {
         request(app).get('/primes/902677838694299069').expect(200).then((response) => {
             expect(response.body.result).toBe(true);
             done();
-        })
-    })
+        });
+    });
 
-    test("a 128-bit prime returns true", (done) => {
+    test('a 128-bit prime returns true', (done) => {
         request(app).get('/primes/311374719183590155288120852969307854859').expect(200).then((response) => {
             expect(response.body.result).toBe(true);
             done();
-        })
-    })
-})
+        });
+    });
+
+    test('a 256-bit prime returns true', (done) => {
+        request(app).get('/primes/29745541885929814667398675890476856124058836135449355131710160267071282930881').expect(200).then((response) => {
+            expect(response.body.result).toBe(true);
+            done();
+        });
+    });
+
+    test('non-numeric values return false', (done) => {
+        request(app).get('/primes/foo').expect(200).then((response) => {
+            expect(response.body.result).toBe(false);
+            done();
+        });
+    });
+});
