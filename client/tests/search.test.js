4e730e154e9be31ad864b57f786eb8555cd8d83e
Casey Billett <cbillett@xyu.ca>
2019-02-15 01:34:28 -0500

    Adding test for client throttling functionality

--- client/tests/search.test.js
+++ client/tests/search.test.js
@@ -4,11 +4,31 @@ const search = require('../src/search');
 jest.mock('axios');
 
 describe('test search functionality', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
     test('searches the right number of times', async (done) => {
         axios.get.mockResolvedValue({ data: { number: 7, result: false } });
         var queue = await search('1', '10');
         queue.drained(() => {
             expect(axios.get).toHaveBeenCalledTimes(10);
+            done();
+        });
+    });
+
+    test('test search triggers throttling', async (done) => {
+        jest.setTimeout(60000);
+        var consoleLog = console.log;
+        var consoleDebug = console.debug;
+        console.log = () => {};
+        console.debug = () => {};
+        axios.get.mockResolvedValue({ data: { number: 7, result: false } });
+        var queue = await search('1', '20000');
+        queue.drained(() => {
+            console.log = consoleLog;
+            console.debug = consoleDebug;
+            expect(axios.get).toHaveBeenCalledTimes(20000);
             done();
         });
     });
